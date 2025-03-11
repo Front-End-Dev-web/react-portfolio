7d3eebb78b359b72b6cfda9e7745f2c4dbef2e6b
Front-End-Dev-web <wa5236759@gmail.com>
2025-03-11 20:06:31 +0500

    d

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 
 import './App.css'
 import Header from './components/header'
-import Lgo from './assets/container-top-img/logo.png'
+
 
 
 function App() {
@@ -10,24 +10,8 @@ function App() {
 
   return (
     <>
- <div id="container-top">
-    <div class="px-4 py-5 my-5 text-center">
-      <img class="d-block mx-auto mb-4 img-fluid" src={Lgo} alt="" width="350"
-        height="110"/>
 
-      <div class="col-lg-6 col-sm-12 mx-auto">
 
-        <div class="d-grid gap-2 d-flex justify-content-center">
-          <a href="#" class="gap-2"><i class="fa-brands fa-facebook-f"></i></a>
-          <a href="#" class="px-3"><i class="fa-brands fa-twitter"></i></a>
-
-          <a href="#" class="gap-2"><i class="fa-brands fa-vimeo-v"></i></a>
-          <a href="#" class="px-3"><i class="fa-brands fa-linkedin-in"></i></a>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="container">
 
 
 
@@ -36,7 +20,7 @@ function App() {
 <Header />
 
 
-</div>
+
     </>
   )
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,60 +1,13 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
-import '../app.css'
+
 export default function Header(){
     return(
         <>
         
         
-
-  
-      <div>
-        <div id="header" >
-          <nav className="navbar navbar-expand-lg navbar-light  py-4 w-100">
-            <div className="container-fluid">
-              <a className="navbar-brand" href="#" />
-              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon" />
-              </button>
-              <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
-                  <li className="nav-item">
-                    <a className="nav-link " aria-current="page" href="#">HOME</a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="#">FEATURES</a>
-                  </li>
-                  <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      BLOG 
-                    </a>
-                    <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                      <li><a className="dropdown-item" href="#">Action</a></li>
-                      <li><a className="dropdown-item" href="#">Another action</a></li>
-                      <li>
-                        <hr className="dropdown-divider" />
-                      </li>
-                      <li><a className="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link " href="#">ABOUT US</a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link " href="#">CONTACT</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-        </div>
-        <div className="search-bar w-100 d-flex justify-content-center align-items-center">
-          <div className="form-floating my-2 w-50">
-            <input type="search" style={{width:'100%'}} className="form-control py-0" placeholder="search here..." />
-            <span><FontAwesomeIcon icon={faSistrix} style={{fontSize:'20px'}}/></span>
-          </div>
-        </div>
-      </div>
+<header>
+  hello world
+</header>
+     
   
         
         
