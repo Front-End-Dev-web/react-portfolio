ce1b91a50ef2fc71b1bd412661baf633d514b4d9
Front-End-Dev-web <wa5236759@gmail.com>
2025-03-12 08:09:26 +0500

    first commit

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,14 +1,41 @@
-
+import '../index.css'
 export default function Header(){
     return(
         <>
-        
-        
-<header>
-  hello world
+        <header id="header">
+  <nav id="navbar" className="navbar">
+    <div className="nav_logo">
+      <img src="application.png" alt="" />
+      <h4> Portfolio</h4>
+    </div>
+    <div className="nav-links">
+      <ul>
+        <li className="li-item">
+          <a href="#">Home</a>
+        </li>
+        <li className="li-item">
+          <a href="#about">About Us</a>
+        </li>
+        <li className="li-item">
+          <a href="#services">Service</a>
+        </li>
+        <li className="li-item">
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li className="li-item">
+          <a href="#blogs">Blog</a>
+        </li>
+      </ul>
+    </div>
+    <div className="nav-button">
+      <button>Contact</button>
+    </div>
+  </nav>
 </header>
-     
-  
+
+        
+        
+        
         
         
         </>
