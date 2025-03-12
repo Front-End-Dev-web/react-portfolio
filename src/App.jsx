5c9dab913260a47bd910864ea3035bb7fff63e6c
Front-End-Dev-web <wa5236759@gmail.com>
2025-03-12 12:46:28 +0500

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
-import { BrowserRouter as Router,Routes, Route } from 'react-router-dom'
+
 
 import './App.css'
-import Main from './main'
+
 import Page from './Page'
 
 function App() {
   
   return (
     <>
-<Router >
-<Routes>
-<Route path='/' element ={<Page/>}/>
-
-
-</Routes>
-</Router>
+<Page/>
     </>
   )
 }
